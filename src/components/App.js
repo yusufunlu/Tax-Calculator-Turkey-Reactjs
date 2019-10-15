79b7dc68b968c08cd944840b215d3f0b2358861f
MOBYUNLU <mobyunlu@thy.com>
2019-10-15 18:06:50 +0300

    removed the unnecessary component imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import '../styles/App.css';
-import FetchComp from './FetchComp'
-import ReadmeShower from './ReadmeShower'
 import VergiForm from './VergiForm'
 
 function App() {
