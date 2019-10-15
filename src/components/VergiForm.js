fd0fb12170a13b32dd95474b4c3339ee4c1090c6
MOBYUNLU <mobyunlu@thy.com>
2019-10-15 21:54:56 +0300

    used material UI instead of plain css and html

--- src/components/VergiForm.js
+++ src/components/VergiForm.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 import '../styles/VergiForm.css';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  root: {
+    width: '40%',
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 450,
+  },
+});
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
 
 class VergiForm extends Component {
+  
   constructor(props) {
     super(props);
+    
     this.state = {
       aylikCalismaGunSayisi : 21,
       gelirKdvsizGunluk: 0,
@@ -75,34 +107,48 @@ class VergiForm extends Component {
   }
   
   render() {
-    const { tasks } = this.state;
+    const { classes } = this.props;
     return (
       <div>
+      <Paper className={classes.root}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>       
+              KDV'siz gunluk gelir: <input type="number" onChange={this.handleChangeGelirGunluk} />
+            </TableCell>
+            <TableCell >          
+            KDV'siz aylik gelir:<input type="number" onChange={this.handleChangeGelirAylik} />
+          </TableCell>
+            <TableCell >          
+              KDV'siz yillik gelir: <input type="number" onChange={this.handleChangeGelirYillik} />
+          </TableCell>
+            <TableCell >          
+              Aylık KDV'siz gider: <input type="number" onChange={this.handleChangeGider} /></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell >{row.calories}</TableCell>
+              <TableCell >{row.fat}</TableCell>
+              <TableCell >{row.carbs}</TableCell>
+              <TableCell >{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
       <form>
-        <label>
-          KDV'siz gunluk gelir:
-          <input type="number" onChange={this.handleChangeGelirGunluk} />
-        </label>
-        <label>
-          KDV'siz aylik gelir:
-          <input type="number" onChange={this.handleChangeGelirAylik} />
-        </label>
-        <label>
-          KDV'siz yillik gelir:
-          <input type="number" onChange={this.handleChangeGelirYillik} />
-        </label>
-        <label>
-          Aylık KDV'siz gider:
-          <input type="number" onChange={this.handleChangeGider} />
-        </label>
-        <br></br>
         <span> Yıllık Gelir: {this.state.gelirKdvli}</span>
         <span> Yıllık Gelir: {this.state.gelirKdvli}</span>
         <span> Yıllık Gelir: {this.state.gelirKdvli}</span>
-        <input type="submit" value="Submit" />
       </form>
     </div>
     );
   }
 }
-export default VergiForm;
+export default withStyles(useStyles)(VergiForm);
