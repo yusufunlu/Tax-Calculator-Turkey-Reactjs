56dc35bb0ad24fc8cf45782d4f514d2fda55d8c9
MOBYUNLU <mobyunlu@thy.com>
2019-11-05 16:57:55 +0300

    High chart added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import VergiForm from './VergiForm'
+import LineChart from './LineChart'
 
 function App() {
   return (
     <div className="App">
       <VergiForm/>
+      <LineChart/>
     </div>
   );
 }
